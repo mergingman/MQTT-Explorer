864ed8b501b49bcef0897475ad23e29990a8359c
Fabian Meyer <fabian.meyer@bbs2.schule>
2023-05-24 09:03:32 +0200

    add mqtt test

---
+++ MQTT-Explorer/MQTT.cs
@@ -0,0 +1,174 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System.Text;
+using System.Text.Json;
+using System.Xml.Linq;
+
+
+namespace MQTT_Explorer
+{
+    public static class MQTT
+    {
+        private static TObject DumpToConsole<TObject>(this TObject @object)
+        {
+            var output = "NULL";
+            if (@object != null)
+            {
+                output = JsonSerializer.Serialize(@object, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+            }
+
+            Console.WriteLine($"[{@object?.GetType().Name}]:\r\n{output}");
+            return @object;
+        }
+
+        public static async Task Handle_Received_Application_Message(string topic, string networkAddress)
+        {
+            //broker.hivemq.com
+            /*
+             * This sample subscribes to a topic and processes the received message.
+             */
+
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(networkAddress).WithWillQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce).Build();
+
+                // Setup message handling before connecting so that queued messages
+                // are also handled properly. When there is no event handler attached all
+                // received messages get lost.
+                mqttClient.ApplicationMessageReceivedAsync += e =>
+                {
+                    Console.WriteLine("Received application message.");
+                    e.DumpToConsole();
+
+                    Console.WriteLine("Message: " + Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment));
+
+                    return Task.CompletedTask;
+                };
+
+                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder()
+                    .WithTopicFilter(
+                        f =>
+                        {
+                            f.WithTopic(topic);
+                        })
+                    .Build();
+
+                await mqttClient.SubscribeAsync(mqttSubscribeOptions, CancellationToken.None);
+
+                Console.WriteLine("MQTT client subscribed to topic: " + topic);
+
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        public static async Task Publish_Application_Message(string topic, string message, string networkAddress)
+        {
+            /*
+             * This sample pushes a simple application message including a topic and a payload.
+             *
+             * Always use builders where they exist. Builders (in this project) are designed to be
+             * backward compatible. Creating an _MqttApplicationMessage_ via its constructor is also
+             * supported but the class might change often in future releases where the builder does not
+             * or at least provides backward compatibility where possible.
+             */
+            ArgumentNullException.ThrowIfNull(message);
+
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder()
+                    .WithTcpServer(networkAddress)
+                    .Build();
+
+                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                var applicationMessage = new MqttApplicationMessageBuilder()
+                    .WithTopic(topic)
+                    .WithPayload(message)
+                    .Build();
+
+                await mqttClient.PublishAsync(applicationMessage, CancellationToken.None);
+
+                await mqttClient.DisconnectAsync();
+
+                Console.WriteLine("MQTT application message " + message + " is published.");
+            }
+        }
+
+        public static void Reconnect_Using_Timer(string networkAddress)
+        {
+            /*
+             * This sample shows how to reconnect when the connection was dropped.
+             * This approach uses a custom Task/Thread which will monitor the connection status.
+             * This is the recommended way but requires more custom code!
+             */
+
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(networkAddress).Build();
+
+                _ = Task.Run(
+                    async () =>
+                    {
+                        // User proper cancellation and no while(true).
+                        while (true)
+                        {
+                            try
+                            {
+                                // This code will also do the very first connect! So no call to _ConnectAsync_ is required in the first place.
+                                if (!await mqttClient.TryPingAsync())
+                                {
+                                    await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                                    // Subscribe to topics when session is clean etc.
+                                    Console.WriteLine("The MQTT client is connected.");
+                                }
+                            }
+                            catch
+                            {
+                                // Handle the exception properly (logging etc.).
+                            }
+                            finally
+                            {
+                                // Check the connection state every 5 seconds and perform a reconnect if required.
+                                await Task.Delay(TimeSpan.FromSeconds(5));
+                            }
+                        }
+                    });
+            }
+        }
+
+        public static async Task Clean_Disconnect(string networkAddress)
+        {
+            /*
+             * This sample disconnects in a clean way. This will send a MQTT DISCONNECT packet
+             * to the server and close the connection afterwards.
+             *
+             * See sample _Connect_Client_ for more details.
+             */
+
+            var mqttFactory = new MqttFactory();
+
+            using (var mqttClient = mqttFactory.CreateMqttClient())
+            {
+                var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer(networkAddress).Build();
+                await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+                // This will send the DISCONNECT packet. Calling _Dispose_ without DisconnectAsync the 
+                // connection is closed in a "not clean" way. See MQTT specification for more details.
+                await mqttClient.DisconnectAsync(new MqttClientDisconnectOptionsBuilder().WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection).Build());
+            }
+        }
+    }
+}
